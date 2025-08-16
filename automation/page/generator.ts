7d43415e00ad7f5048c57352cb7547a45d46c7b7
Lucas Ramos <lucasnramos@gmail.com>
2025-08-16 13:56:54 -0300

    first setup for page generator

--- automation/page/generator.ts
+++ automation/page/generator.ts
@@ -2,6 +2,7 @@ import {
   addProjectConfiguration,
   formatFiles,
   generateFiles,
+  names,
   Tree,
 } from '@nx/devkit';
 import * as path from 'path';
@@ -11,14 +12,15 @@ export async function generatorGenerator(
   tree: Tree,
   options: GeneratorGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
+  const projectRoot = `src/pages/${options.name}`;
+  const _names = names(options.name);
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
     projectType: 'library',
     sourceRoot: `${projectRoot}/src`,
     targets: {},
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, _names);
   await formatFiles(tree);
 }
 
